e31a45f8eaab0a2cb129bcad20e2510a5621c64b
Simon RENOULT <simon.renoult@gmail.com>
2013-04-11 14:51:47 +0200

    fix syntax

--- app/autoloader.php
+++ app/autoloader.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 function autoloader ( $className ) {
     $file = $className . CLASS_EXTENSION;
@@ -11,12 +11,12 @@ spl_autoload_register ( 'autoloader' );
 // spl_autoload_register ( 'autoloader2' );
 
 function autoloader2 ( $className ) {
-    $paths = [
+    $paths = array (
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'controllers',
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'models',
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'tools',
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'exceptions'
-    ];
+    );
     
     foreach ( $paths as $path ) {
         @require_once ( $path . DIRECTORY_SEPARATOR . $className . CLASS_EXTENSION );

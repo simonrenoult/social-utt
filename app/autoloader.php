c37654af7603bef490b02ea11d6bf0622aa1500a
Simon RENOULT <simon.renoult@gmail.com>
2013-04-12 10:50:12 +0200

    Revert "fix syntax"
    This reverts commit e31a45f8eaab0a2cb129bcad20e2510a5621c64b.

--- app/autoloader.php
+++ app/autoloader.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 function autoloader ( $className ) {
     $file = $className . CLASS_EXTENSION;
@@ -11,12 +11,12 @@ spl_autoload_register ( 'autoloader' );
 // spl_autoload_register ( 'autoloader2' );
 
 function autoloader2 ( $className ) {
-    $paths = array (
+    $paths = [
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'controllers',
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'models',
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'tools',
        dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'exceptions'
-    );
+    ];
     
     foreach ( $paths as $path ) {
         @require_once ( $path . DIRECTORY_SEPARATOR . $className . CLASS_EXTENSION );

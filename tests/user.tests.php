8ed500518e6bafc4438b2be86864ce498fa3e990
Simon RENOULT <simon.renoult@gmail.com>
2013-04-28 11:44:43 +0200

    update project

--- tests/user.tests.php
+++ tests/user.tests.php
@@ -1,8 +1,21 @@
 <?php
-  
-  require_once ( '../app/autoloader.php' );
-  
-  use app\models as Models;
-  use app\controllers as Controllers;
+
+define ( 'EXTENSION', '.php' );
+define ( 'CLASS_EXTENSION', '.class' . EXTENSION );  
+
+require_once ( '../app/autoloader.php' );
+require_once ( '../app/tools/Misc.php' );
+
+$conn = app\tools\DatabaseConnector::getConnection ( );
+
+$user = new app\models\User ( array ( 
+    "login" => generateRandomString ( ),
+    "password" => generateRandomString ( ),
+    "email" => generateRandomString ( ) . "@test.com"
+) );
+
+$user_manager = new app\controllers\UserManager ( $conn, $user );
+
+
 
 ?>

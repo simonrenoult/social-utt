0b8bd990d8f15c38e0e7f87f73a6e9357d9f5eb0
Simon RENOULT <simon.renoult@gmail.com>
2013-04-28 11:46:06 +0200

    update all

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
  
-    define ( 'EXTENSION', '.php' );
-    define ( 'CLASS_EXTENSION', '.class' . EXTENSION );
-    
-    require_once ( './app/autoloader.php' );
-  
-    use app\models as Models;
-    use app\controllers as Controllers;
-    use app\tools as Tools;
-    
-    $conn = Tools\DatabaseConnector::getConnection ( );
-    $user = new Models\User ( array ( 
-        "login" => "simon",
-        "email" => "simon@gmail.com",
-        "password" => "test"
-    ) );
+define ( 'EXTENSION', '.php' );
+define ( 'CLASS_EXTENSION', '.class' . EXTENSION );
+define ( 'PATH', __DIR__ . DIRECTORY_SEPARATOR );
 
-    $manager = new Controllers\UserManager ( $conn, $user );
-    $status = $manager -> save ();
-    
-    var_dump ( $status -> toArray ( ) );
+require_once ( PATH . 'app/autoloader.php' );
+
+use app\models as Models;
+use app\controllers as Controllers;
+use app\tools as Tools;
+
+$conn = Tools\DatabaseConnector::getConnection ( );
+$user = new Models\User ( array ( 
+    "login" => "simon",
+    "email" => "simon@gmail.com",
+    "password" => "test"
+) );
+
+$manager = new Controllers\UserManager ( $conn, $user );
+$status = $manager -> save ( );
+
+var_dump ( $status -> toArray ( ) );
 ?>
